160e65b499f6582cc5da57d7f16c1fa6b9895c71
zhanghaonan <zhanghaonan@capinfo.com.cn>
2020-06-03 18:24:00 +0800

    初始化第二个算法题

---
+++ no2/goland/sol1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("hello world")
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	return nil
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
